0771a9cc649445120be0af9deb29440677df12c1
GitDogBar <Dogbar>
2021-08-23 03:26:41 +0700

    Connect DataBase

---
+++ Data/DbContextCustomer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_Test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Test.Data
+{
+    public class DbContextCustomer : DbContext
+    {
+        public DbContextCustomer(DbContextOptions<DbContextCustomer>options ):base(options)
+        {
+            
+        }
+        public DbSet<Customers> customersTbl { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Project_Test.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,7 @@ namespace Project_Test
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DbContextCustomer>(options => options.UseSqlServer(Configuration.GetConnectionString("ConnectDev")));
             services.AddControllersWithViews();
         }
 
